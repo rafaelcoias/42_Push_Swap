d18b09f891bfba2cab8bf12257a907edcfe9918f
rafaelcoias <rafaelmsppcoias@gmail.com>
2022-04-09 09:50:05 +0100

    improved 2

--- push_swap/src/sort_hundreds.c
+++ push_swap/src/sort_hundreds.c
@@ -14,13 +14,12 @@
 
 void	sort_hundreds(t_stack **a, t_stack **b)
 {
-    while (get_smallest(*a) != get_biggest(*a))
+    while (get_size(*a) > 3)
     {
         put_at_top_a(a, get_smallest(*a));
         do_pb(a, b);
     }
-	while (get_size(*a))
-		do_pb(a, b);
+	sort_three(a);
 	while (get_size(*b))
 	{
 		put_at_top_b(b, get_biggest(*b));
